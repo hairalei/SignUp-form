94fa4d917d8056666da1f29ab15c1f2fc5f269ca
hairalei <darhaii13@gmail.com>
2022-05-04 16:45:20 +0800

    Add password match function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+function matchPassword(e) {
+  const password1 = document.querySelector("#password1");
+  const password2 = document.querySelector("#password2");
+  const error = document.querySelector(".error");
+  e.preventDefault();
+  if (password1.value !== password2.value) error.style.display = "block";
+}
+
+document.querySelector(".btn").addEventListener("click", matchPassword);
